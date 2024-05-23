17cc10e5d8f99b13f6a10938bfdc809005ac3dfe
jus1d <artfa63@gmail.com>
2024-05-23 21:49:52 +0400

    chore: cleanup code

--- telegram/telegram.go
+++ telegram/telegram.go
@@ -20,10 +20,10 @@ const (
 )
 
 type Bot struct {
-	IsDebugMode bool
-	Games       map[int64]*terminal.Game
-	Stages      map[int64]Stage
-	*tgbotapi.BotAPI
+	isDebugMode bool
+	games       map[int64]*terminal.Game
+	stages      map[int64]Stage
+	bot         *tgbotapi.BotAPI
 }
 
 func New(token string, isDebugMode bool) *Bot {
@@ -33,78 +33,78 @@ func New(token string, isDebugMode bool) *Bot {
 	}
 
 	return &Bot{
-		IsDebugMode: isDebugMode,
-		Games:       make(map[int64]*terminal.Game, 0),
-		Stages:      make(map[int64]Stage, 0),
-		BotAPI:      bot,
+		isDebugMode: isDebugMode,
+		games:       make(map[int64]*terminal.Game, 0),
+		stages:      make(map[int64]Stage, 0),
+		bot:         bot,
 	}
 }
 
-func (bot *Bot) Run() {
-	log.Printf("Authorized on account @%s", bot.Self.UserName)
+func (b *Bot) Run() {
+	log.Printf("Authorized on account @%s", b.bot.Self.UserName)
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
-	updates := bot.GetUpdatesChan(u)
+	updates := b.bot.GetUpdatesChan(u)
 
 	for update := range updates {
-		bot.handleUpdate(update)
+		b.handleUpdate(update)
 	}
 }
 
-func (bot *Bot) handleUpdate(u tgbotapi.Update) {
+func (b *Bot) handleUpdate(u tgbotapi.Update) {
 	if u.Message != nil {
 		log.Printf("[@%s] says: `%s`", u.Message.From.UserName, u.Message.Text)
 
 		switch u.Message.Text {
 		case "/start":
-			bot.handleStartCommand(u)
+			b.handleStartCommand(u)
 		case "/game":
-			bot.handleGameCommand(u)
+			b.handleGameCommand(u)
 		default:
-			bot.handleTextMessage(u)
+			b.handleTextMessage(u)
 		}
 	}
 }
 
-func (bot *Bot) handleStartCommand(u tgbotapi.Update) {
+func (b *Bot) handleStartCommand(u tgbotapi.Update) {
 	userID := u.Message.From.ID
-	bot.sendMessage(userID, "Hello, Im gonna help you with your TERMINAL games. Use /game to start")
-	bot.Stages[userID] = None
+	b.sendMessage(userID, "Hello, Im gonna help you with your TERMINAL games. Use /game to start")
+	b.stages[userID] = None
 }
 
-func (bot *Bot) handleGameCommand(u tgbotapi.Update) {
+func (b *Bot) handleGameCommand(u tgbotapi.Update) {
 	userID := u.Message.From.ID
-	_, exists := bot.Games[userID]
+	_, exists := b.games[userID]
 	if exists {
 		// TODO: Add buttons: continue | start new game
-		bot.sendMessage(userID, "[TODO] You already have started game. Do you want to continue?")
+		b.sendMessage(userID, "[TODO] You already have started game. Do you want to continue?")
 		return
 	}
 
-	bot.sendMessage(userID, "Send me list of words in your TERMINAL game")
-	bot.Stages[userID] = WaitingWordList
+	b.sendMessage(userID, "Send me list of words in your TERMINAL game")
+	b.stages[userID] = WaitingWordList
 }
 
-func (bot *Bot) handleTextMessage(u tgbotapi.Update) {
+func (b *Bot) handleTextMessage(u tgbotapi.Update) {
 	userID := u.Message.From.ID
-	stage, exists := bot.Stages[userID]
+	stage, exists := b.stages[userID]
 	if exists && stage == WaitingWordList {
 		words := strings.Split(u.Message.Text, "\n")
 		game, err := terminal.New(words)
 		if errors.Is(err, terminal.ErrDifferentWordsLength) {
-			bot.sendMessage(userID, "Words should not be different length")
+			b.sendMessage(userID, "Words should not be different length")
 			return
 		}
-		bot.Games[userID] = game
-		bot.Stages[userID] = WaitingAttempt
+		b.games[userID] = game
+		b.stages[userID] = WaitingAttempt
 
-		content := fmt.Sprintf("<b>Available %d words:</b>\n", len(bot.Games[userID].Words))
-		for i, word := range bot.Games[userID].Words {
+		content := fmt.Sprintf("<b>Available %d words:</b>\n", len(b.games[userID].Words))
+		for i, word := range b.games[userID].Words {
 			content += fmt.Sprintf("#%d: <code>%s</code>\n", i+1, word)
 		}
-		bot.sendMessage(userID, content)
+		b.sendMessage(userID, content)
 	} else if exists && stage == WaitingAttempt {
 		parts := strings.Split(u.Message.Text, " ")
 		word := parts[0]
@@ -113,29 +113,29 @@ func (bot *Bot) handleTextMessage(u tgbotapi.Update) {
 			Word:           word,
 			GuessedLetters: guessedLetters,
 		}
-		bot.Games[userID].Attempts = append(bot.Games[userID].Attempts, &attempt)
-		bot.Games[userID].UpdateWords()
-		content := fmt.Sprintf("<b>Available %d words:</b>\n", len(bot.Games[userID].Words))
-		for i, word := range bot.Games[userID].Words {
+		b.games[userID].Attempts = append(b.games[userID].Attempts, &attempt)
+		b.games[userID].UpdateWords()
+		content := fmt.Sprintf("<b>Available %d words:</b>\n", len(b.games[userID].Words))
+		for i, word := range b.games[userID].Words {
 			content += fmt.Sprintf("#%d: <code>%s</code>\n", i+1, word)
 		}
-		bot.sendMessage(userID, content)
-		if len(bot.Games[userID].Words) == 1 {
-			bot.Stages[userID] = None
+		b.sendMessage(userID, content)
+		if len(b.games[userID].Words) == 1 {
+			b.stages[userID] = None
 		}
 	}
 }
 
-func (bot *Bot) sendMessage(chatID int64, content string) {
+func (b *Bot) sendMessage(chatID int64, content string) {
 	message := tgbotapi.NewMessage(chatID, content)
 	message.ParseMode = tgbotapi.ModeHTML
 
-	_, err := bot.Send(message)
+	_, err := b.bot.Send(message)
 	if err != nil {
 		log.Printf("ERROR: could not send message to ID: %d, error: %s\n", chatID, err.Error())
 		return
 	}
-	if bot.IsDebugMode {
+	if b.isDebugMode {
 		log.Printf("DEBUG: message sent to ID: %d, content: %s\n", chatID, content)
 	}
 }
