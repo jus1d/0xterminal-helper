1f43c878ceaee1b12e6f6c9b92f93f9c4e9d7c2c
jus1d <artfa63@gmail.com>
2024-05-29 21:29:23 +0400

    Add TODO(#10)

--- internal/telegram/handler/callback.go
+++ internal/telegram/handler/callback.go
@@ -111,7 +111,7 @@ func (h *Handler) CallbackChooseGuessedLetters(u tgbotapi.Update) {
 
 		// we'll assume that game is kinda spam, if initial words is less than 6
 		if len(game.InitialWords) >= 6 {
-			// TODO: add initial words sorting
+			// TODO(#10): add initial words sorting
 			h.storage.SaveGame(userID, game.InitialWords, game.AvailableWords[0])
 		}
 		return
