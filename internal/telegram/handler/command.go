324144da0c725a58021881bc786703a3d20415df
jus1d <artfa63@gmail.com>
2024-06-05 10:45:51 +0400

    fix: bold font in daily report

--- internal/telegram/handler/command.go
+++ internal/telegram/handler/command.go
@@ -155,7 +155,7 @@ func (h *Handler) CommandDailyReport(u tgbotapi.Update) {
 		}
 	}
 
-	content = fmt.Sprintf("<b>%s</b>\n\n<b>Games played: %d</b>\n", time.Now().Format("2 January, 2006"), totalGames) + content
+	content = fmt.Sprintf("<b>%s</b>\n\n<b>Games played:</b> %d\n", time.Now().Format("2 January, 2006"), totalGames) + content
 
 	content += fmt.Sprintf("<b>Joined users:</b> %d\n", len(report.JoinedUsers))
 	for _, user := range report.JoinedUsers {
